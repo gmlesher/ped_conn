15de0d455965ef3091066562438ac3353139b53a
gmlesher <glesherlhs@gmail.com>
2021-01-29 17:42:36 -0500

    fix heroku

--- pedcon/settings.py
+++ pedcon/settings.py
@@ -95,7 +95,8 @@ WSGI_APPLICATION = 'pedcon.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
+        # 'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
@@ -137,9 +138,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-    )
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILE_DIRS = [
+      "static/images", 
+      "static/css",
+      "static/js",
+      "staticfiles",]
+
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+#     )
 
 # Google reCAPTCHA keys
 # these are testing keys. not real ones. get real ones for production
