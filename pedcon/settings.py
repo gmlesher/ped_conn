dc1543d8371a0a3f710e5220b7f2f924e6adf457
gmlesher <glesherlhs@gmail.com>
2021-01-30 15:02:33 -0500

    redo again

--- pedcon/settings.py
+++ pedcon/settings.py
@@ -105,15 +105,15 @@ WSGI_APPLICATION = 'pedcon.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
-#         # 'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-DATABASES = {}
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+# DATABASES = {}
+# DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 
 # Password validation
