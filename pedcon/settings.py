06a3815d7256c7bf1a36ae52bf912ec164c0393b
gmlesher <glesherlhs@gmail.com>
2021-02-01 15:37:04 -0500

    added heroku as allowed host

--- pedcon/settings.py
+++ pedcon/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://pediatric-connections.herokuapp.com/']
 
 
 # Application definition
