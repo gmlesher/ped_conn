a31a8dab26dbaacd8fb7e83826b60bd430908008
gmlesher <glesherlhs@gmail.com>
2021-02-04 11:33:59 -0500

    updated settings for media uploads to aws s3. added storage_backends.py

--- pedcon/settings.py
+++ pedcon/settings.py
@@ -144,17 +144,17 @@ USE_L10N = True
 
 USE_TZ = True
 
-# AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
-# AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
-# AWS_STORAGE_BUCKET_NAME = 'pediatric-connections-bucket'
-# AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-# AWS_S3_OBJECT_PARAMETERS = {
-#     'CacheControl': 'max-age=86400',
-# }
-# AWS_LOCATION = 'static'
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+AWS_LOCATION = 'media'
 # STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 # STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-# DEFAULT_FILE_STORAGE = 'pedcon.storage_backends.MediaStorage'
+DEFAULT_FILE_STORAGE = 'pedcon.storage_backends.MediaStorage'
 
 
 # Static files (CSS, JavaScript, Images)
