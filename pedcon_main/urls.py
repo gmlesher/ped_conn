3419ffe86005a29dc489457197874acc7b553d02
gmlesher <glesherlhs@gmail.com>
2022-05-26 21:36:00 -0500

    remove employee

--- pedcon_main/urls.py
+++ pedcon_main/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 app_name = 'pedcon_main'
 urlpatterns = [
-    
+
     # Home Page
     path('', views.index, name='index'),
     # About us
@@ -17,11 +17,13 @@ urlpatterns = [
     # Faq page
     path('faq/', views.faq, name='faq'),
     # Local professionals page
-    path('local_professionals/', views.local_professionals, name='local_professionals'),
+    path('local_professionals/', views.local_professionals,
+         name='local_professionals'),
     # Helpful links page
     path('helpful_links/', views.helpful_links, name='helpful_links'),
     # Recommended product page
-    path('recommended_products/', views.recommended_products, name='recommended_products'),
+    path('recommended_products/', views.recommended_products,
+         name='recommended_products'),
     # Contact page
     path('contact/', views.contact, name='contact'),
     # Privacy policy page
@@ -35,8 +37,6 @@ urlpatterns = [
 
     # About Deana page
     path('about_us_deana', views.about_us_deana, name='about_us_deana'),
-    # About Andrea page
-    path('andrea-arlotti', views.andrea_arlotti, name='andrea-arlotti'),
     # About Lisa berry page
     path('lisa-berry', views.lisa_berry, name='lisa-berry'),
     # About Briana page
@@ -75,15 +75,19 @@ urlpatterns = [
     # Client information form page
     path('client_info_form/', views.CIFormView.as_view(), name='client_info_form'),
     # Payment policy form page
-    path('payment_policy_form/', views.PaymentPolicyFormView.as_view(), name='payment_policy_form'),
+    path('payment_policy_form/', views.PaymentPolicyFormView.as_view(),
+         name='payment_policy_form'),
     # Cancellation policy form page
-    path('cancellation_policy_form/', views.CancellationPolicyFormView.as_view(), name='cancellation_policy_form'),
+    path('cancellation_policy_form/', views.CancellationPolicyFormView.as_view(),
+         name='cancellation_policy_form'),
     # Pediatric history form page
-    path('pediatric_hx_form/', views.PediatricHxFormView.as_view(), name='pediatric_hx_form'),
+    path('pediatric_hx_form/', views.PediatricHxFormView.as_view(),
+         name='pediatric_hx_form'),
     # Adult history form page
     path('adult_hx_form/', views.AdultHxFormView.as_view(), name='adult_hx_form'),
     # Speech Language history form page
-    path('speech_language_hx_form/', views.SpeechLanguageHxFormView.as_view(), name='speech_language_hx_form'),
+    path('speech_language_hx_form/', views.SpeechLanguageHxFormView.as_view(),
+         name='speech_language_hx_form'),
     # Release of information history form page
     path('roi_form/', views.ROIFormView.as_view(), name='roi_form'),
     # SSP form page
@@ -102,18 +106,23 @@ urlpatterns = [
     # Payment policy PDF view
     path('pp_pdf_view/<int:pk>/', views.ViewPPPDF.as_view(), name='pp_pdf_view'),
     # Cancellation Policy PDF view
-    path('cancel_pdf_view/<int:pk>/', views.ViewCancelPDF.as_view(), name='cancel_pdf_view'),
+    path('cancel_pdf_view/<int:pk>/',
+         views.ViewCancelPDF.as_view(), name='cancel_pdf_view'),
     # ROI PDF view
     path('roi_pdf_view/<int:pk>/', views.ViewROIPDF.as_view(), name='roi_pdf_view'),
     # Pediatric History PDF view
-    path('pediatric_hx_pdf_view/<int:pk>/', views.ViewPedHxPDF.as_view(), name='pediatric_hx_pdf_view'),
+    path('pediatric_hx_pdf_view/<int:pk>/',
+         views.ViewPedHxPDF.as_view(), name='pediatric_hx_pdf_view'),
     # Adult History PDF view
-    path('adult_hx_pdf_view/<int:pk>/', views.ViewAdultHxPDF.as_view(), name='adult_hx_pdf_view'),
+    path('adult_hx_pdf_view/<int:pk>/',
+         views.ViewAdultHxPDF.as_view(), name='adult_hx_pdf_view'),
     # Speech Language History PDF view
-    path('speech_lang_hx_pdf_view/<int:pk>/', views.ViewSpeechLangHxPDF.as_view(), name='speech_lang_hx_pdf_view'),
+    path('speech_lang_hx_pdf_view/<int:pk>/',
+         views.ViewSpeechLangHxPDF.as_view(), name='speech_lang_hx_pdf_view'),
     # Safe and Sound PDF view
     path('ssp_pdf_view/<int:pk>/', views.ViewSSPPDF.as_view(), name='ssp_pdf_view'),
     # Hipaa PDF view
-    path('hipaa_pdf_view/<int:pk>/', views.ViewHipaaPDF.as_view(), name='hipaa_pdf_view'),
-    
+    path('hipaa_pdf_view/<int:pk>/',
+         views.ViewHipaaPDF.as_view(), name='hipaa_pdf_view'),
+
 ]
--- pedcon_main/views.py
+++ pedcon_main/views.py
@@ -1,16 +1,16 @@
-""" django file imports:"""
-from django.http import HttpResponseRedirect, HttpResponse
-from django.shortcuts import render
-from django.core import validators
-from django.core.mail import EmailMessage # for sending email in contact page
-from django.conf import settings # for sending email in contact page
-from django.http import Http404 # allows me to force 404 page when needed
-from django.views.generic import View # for class based views 
-
-""" my file imports: """
+# My file imports:
 from .forms import *
 from .models import *
 from .utils import *
+
+# Django file imports:
+from django.views.generic import View  # for class based views
+from django.http import Http404  # allows me to force 404 page when needed
+from django.conf import settings  # for sending email in contact page
+from django.core.mail import EmailMessage  # for sending email in contact page
+from django.core import validators
+from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponse
 
 
 # # Practice form processing (see .utils for Mixin)
@@ -26,8 +26,6 @@ from .utils import *
 #     template = 'pedcon_main/practice_pdf_template.html'
 
 
-
-
 # Client info form processing (see .utils for Mixin)
 class CIFormView(ProcessFormMixin, View):
     form_class = ClientInformationForm
@@ -36,11 +34,11 @@ class CIFormView(ProcessFormMixin, View):
     to_url = '/ci_pdf_view/'
 
 # renders client information to PDF & sends email of PDF
+
+
 class ViewCIPDF(CreatePdfMixin, EmailPdfMixin, View):
     model = ClientInformation
     template = 'pedcon_main/client_info_pdf_template.html'
-
-
 
 
 # Payment policy form processing (see .utils for Mixin)
@@ -51,11 +49,11 @@ class PaymentPolicyFormView(ProcessFormMixin, View):
     to_url = '/pp_pdf_view/'
 
 # renders payment policy information to PDF & sends email of PDF
+
+
 class ViewPPPDF(CreatePdfMixin, EmailPdfMixin, View):
     model = PaymentPolicyInformation
     template = 'pedcon_main/payment_policy_pdf_template.html'
-
-
 
 
 # Cancellation policy form processing (see .utils for Mixin)
@@ -66,11 +64,11 @@ class CancellationPolicyFormView(ProcessFormMixin, View):
     to_url = '/cancel_pdf_view/'
 
 # renders cancellation policy information to PDF & sends email of PDF
+
+
 class ViewCancelPDF(CreatePdfMixin, EmailPdfMixin, View):
     model = CancellationPolicyInformation
     template = 'pedcon_main/cancellation_pdf_template.html'
-
-
 
 
 # Pediatric history form processing (see .utils for Mixin)
@@ -81,11 +79,11 @@ class PediatricHxFormView(ProcessFormMixin, View):
     to_url = '/pediatric_hx_pdf_view/'
 
 # renders pediatric history information to PDF & sends email of PDF
+
+
 class ViewPedHxPDF(CreatePdfMixin, EmailPdfMixin, View):
     model = PediatricHistoryInformation
     template = 'pedcon_main/pediatric_hx_pdf_template.html'
-
-
 
 
 # Adult history form processing (see .utils for Mixin)
@@ -96,11 +94,11 @@ class AdultHxFormView(ProcessFormMixin, View):
     to_url = '/adult_hx_pdf_view/'
 
 # renders adult history information to PDF & sends email of PDF
+
+
 class ViewAdultHxPDF(CreatePdfMixin, EmailPdfMixin, View):
     model = AdultHistoryInformation
     template = 'pedcon_main/adult_hx_pdf_template.html'
-
-
 
 
 # Speech language history form processing (see .utils for Mixin)
@@ -111,11 +109,11 @@ class SpeechLanguageHxFormView(ProcessFormMixin, View):
     to_url = '/speech_lang_hx_pdf_view/'
 
 # renders speech language history information to PDF & sends email of PDF
+
+
 class ViewSpeechLangHxPDF(CreatePdfMixin, EmailPdfMixin, View):
     model = SpeechLanguageHxInformation
     template = 'pedcon_main/speech_lang_hx_pdf_template.html'
-
-
 
 
 # Release of information form processing (see .utils for Mixin)
@@ -126,11 +124,11 @@ class ROIFormView(ProcessFormMixin, View):
     to_url = '/roi_pdf_view/'
 
 # renders release of information to PDF & sends email of PDF
+
+
 class ViewROIPDF(CreatePdfMixin, EmailPdfMixin, View):
     model = ReleaseOfInformation
     template = 'pedcon_main/roi_pdf_template.html'
-
-
 
 
 # Safe and sound form processing (see .utils for Mixin)
@@ -141,11 +139,11 @@ class SSPFormView(ProcessFormMixin, View):
     to_url = '/ssp_pdf_view/'
 
 # renders SSP information to PDF & sends email of PDF
+
+
 class ViewSSPPDF(CreatePdfMixin, EmailPdfMixin, View):
     model = SafeAndSoundInformation
     template = 'pedcon_main/safe_and_sound_pdf_template.html'
-
-
 
 
 # Hipaa form processing (see .utils for Mixin)
@@ -156,11 +154,11 @@ class HipaaFormView(ProcessFormMixin, View):
     to_url = '/hipaa_pdf_view/'
 
 # renders SSP information to PDF & sends email of PDF
+
+
 class ViewHipaaPDF(CreatePdfMixin, EmailPdfMixin, View):
     model = HipaaInformation
     template = 'pedcon_main/hipaa_pdf_template.html'
-
-
 
 
 # Contact form view. Processes and sends content of contact form
@@ -183,9 +181,10 @@ def contact(request):
                   f'Phone: {message_phone} \n'\
                   f'Subject: {message_subject} \n'\
                   f'Message: {message} \n'
-            recipients = ['info@pediatricconnectionsot.com',]
-            # recipients = ['gmlesher@gmail.com',] 
-            email = EmailMessage(subject, msg, settings.EMAIL_HOST_USER, recipients)
+            recipients = ['info@pediatricconnectionsot.com', ]
+            # recipients = ['gmlesher@gmail.com',]
+            email = EmailMessage(
+                subject, msg, settings.EMAIL_HOST_USER, recipients)
             email.send(fail_silently=True)
 
             '''Use below code for sending to multiple recipients with sendgrid 
@@ -202,105 +201,126 @@ def contact(request):
         form = ContactForm()
     return render(request, 'pedcon_main/contact.html', {'form': form})
 
+
 def index(request):
     """The home page for pedcon."""
     return render(request, 'pedcon_main/index.html')
+
 
 def about_us(request):
     """The about us page for pedcon."""
     return render(request, 'pedcon_main/about_us.html')
 
+
 def about_us_deana(request):
     """The about us Deana page for pedcon."""
     return render(request, 'pedcon_main/about_us_deana.html')
 
-def andrea_arlotti(request):
-    """The about us Andrea Arlotti page for pedcon."""
-    return render(request, 'pedcon_main/andrea-arlotti.html')
 
 def lisa_berry(request):
     """The about us Lisa berry page for pedcon."""
     return render(request, 'pedcon_main/lisa-berry.html')
 
+
 def briana_picht(request):
     """The about us Briana Picht page for pedcon."""
     return render(request, 'pedcon_main/briana-picht.html')
+
 
 def lisa_bergeson(request):
     """The about us Lisa bergeson page for pedcon."""
     return render(request, 'pedcon_main/lisa-bergeson.html')
 
+
 def jennifer_shrum(request):
     """The about us Jennifer Shrum page for pedcon."""
     return render(request, 'pedcon_main/jennifer-shrum.html')
+
 
 def paula_worley(request):
     """The about us Paula Worley page for pedcon."""
     return render(request, 'pedcon_main/paula-worley.html')
 
+
 def trisha_morris(request):
     """The about us Trisha Morris page for pedcon."""
     return render(request, 'pedcon_main/trisha-morris.html')
+
 
 def cathy_pickert(request):
     """The about us Cathy Pickert page for pedcon."""
     return render(request, 'pedcon_main/cathy-pickert.html')
 
+
 def suzanne_remy(request):
     """The about us Suzanne Remy page for pedcon."""
     return render(request, 'pedcon_main/suzanne-remy.html')
+
 
 def jennifer_vennart(request):
     """The about us Jennifer Vennart page for pedcon."""
     return render(request, 'pedcon_main/jennifer-vennart.html')
 
+
 def casey_fitzgerald(request):
     """The about us Casey Fitzgerald page for pedcon."""
     return render(request, 'pedcon_main/casey-fitzgerald.html')
+
 
 def kelly_daugherty(request):
     """The about us Kelly Daugherty page for pedcon."""
     return render(request, 'pedcon_main/kelly-daugherty.html')
 
+
 def kaylan_campain(request):
     """The about us Kaylan Campain page for pedcon."""
     return render(request, 'pedcon_main/kaylan-campain.html')
+
 
 def allison_casey(request):
     """The about us Allison Casey page for pedcon."""
     return render(request, 'pedcon_main/allison-casey.html')
 
+
 def taylor_sjorgen(request):
     """The about us Taylor Sjorgen page for pedcon."""
     return render(request, 'pedcon_main/taylor-sjorgen.html')
+
 
 def services(request):
     """The services page for pedcon."""
     return render(request, 'pedcon_main/services.html')
 
+
 def forms(request):
     """The forms page for pedcon."""
     return render(request, 'pedcon_main/forms.html')
+
 
 def faq(request):
     """The faq page for pedcon."""
     return render(request, 'pedcon_main/faq.html')
 
+
 def local_professionals(request):
     """The local_professionals page for pedcon."""
     return render(request, 'pedcon_main/local_professionals.html')
+
 
 def helpful_links(request):
     """The helpful_links page for pedcon."""
     return render(request, 'pedcon_main/helpful_links.html')
 
+
 def recommended_products(request):
     """The recommended_products page for pedcon."""
     return render(request, 'pedcon_main/recommended_products.html')
 
+
 def privacy_policy(request):
     """The privacy policy page for pedcon."""
     return render(request, 'pedcon_main/privacy_policy.html')
+
 
 def success(request):
     """The success page for pedcon."""
